f957efd53a3153e79669001b7627e9a1dbc28235
sahabatshaff <149377651+sahabatshaff@users.noreply.github.com>
2023-11-16 12:10:08 +0700

    Add files via upload

--- assets/main.js
+++ assets/main.js
@@ -11,6 +11,13 @@
       iconhome.classList.toggle('active');
       navbarlubna.classList.toggle('active');
   }
+  function PlayAudio() {
+      const audioplayer = document.getElementById('audioplayer');
+      const buttonplay = document.getElementById('buttonplay');
+
+      buttonplay.classList.toggle('activeplay');
+      audioplayer.classList.toggle('activeplay');
+  }
 //   document.onclick = function(e){
 //     if(e.target.id !== 'navbarlubna' && e.target.id !== 'iconhome')
 //         {
@@ -18,3 +25,7 @@
 //             navbarlubna.classList.remove('active');
 //         }
 //     }
+
+    document.querySelector('button').addEventListener('click', () => {
+        document.querySelector('audio').play();
+    });
